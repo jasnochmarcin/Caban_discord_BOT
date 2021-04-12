9eb9e2621ffd37e11e8a8d24b41a94ebccf07991
jasnochmarcin <74145933+jasnochmarcin@users.noreply.github.com>
2021-04-12 23:11:12 +0200

    Update main.py
    Updates:
    -delete commands
    -added cogs

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from os import environ
 from CabanBot import CabanBot
 from dotenv import load_dotenv
-from commands.hello import hello
+from cogs.Greetings import Greetings
+from cogs.Memes import Memes
 
 load_dotenv()
 DISCORD_TOKEN = environ.get('DISCORD_TOKEN')
 
 
 bot = CabanBot(command_prefix='$')
-bot.add_command(hello)
+bot.add_cog(Greetings(bot))
+bot.add_cog(Memes(bot))
 bot.run(DISCORD_TOKEN)
